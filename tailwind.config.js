1a2da20e00b2e07d754f1cda32b4b3a4bc4a03e2
Ashish <ashishpise@syntags.co>
2022-02-18 16:25:07 +0530

    added tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    "src/**/*.{html,ts}",
+  ],
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
